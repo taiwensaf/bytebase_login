d286c8b253c6ece794d1f17433c644116327b41e
wwweiii <471747592@qq.com>
2025-04-26 00:46:35 +0800

    repair login setting

--- utils/githubAuth.ts
+++ utils/githubAuth.ts
@@ -1,4 +1,12 @@
+if (!process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID) {
+  console.error('GitHub Client ID is not set. Please check your environment variables.');
+}
+
 export const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID || '';
+
+if (!GITHUB_CLIENT_ID) {
+  console.error('GitHub Client ID is empty. OAuth will not work.');
+}
 
 export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user:email`;
 
